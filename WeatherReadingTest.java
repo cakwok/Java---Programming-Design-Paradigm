f7e155e8aa5105f93f63d915bc0c3d9c66e6b625
Casca Kwok <casca.kwok@gmail.com>
2022-09-21 12:03:32 -0700

    Create WeatherReadingTest.java

---
+++ WeatherReadingTest.java
@@ -0,0 +1,31 @@
+package Weather;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherReadingTest {
+
+  private WeatherReading weatherReading1;
+
+  public void setUp() {
+    weatherReading1 = new WeatherReading(22, 13,  6, 0)
+  }
+  @org.junit.jupiter.api.Test
+  void getTemperature() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getDewPoint() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getWindSpeed() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getTotalRain() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getRelativeHumidity() {
+  }
+}
