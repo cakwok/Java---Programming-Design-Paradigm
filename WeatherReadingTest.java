cf9894e25a0518dbfcff58c518caa95b055fddbc
Casca Kwok <casca.kwok@gmail.com>
2022-09-21 17:46:02 -0700

    Update WeatherReadingTest.java

--- WeatherReadingTest.java
+++ WeatherReadingTest.java
@@ -1,31 +1,53 @@
 package Weather;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
 
-class WeatherReadingTest {
+public class WeatherReadingTest {
 
   private WeatherReading weatherReading1;
 
+  @Before
   public void setUp() {
-    weatherReading1 = new WeatherReading(22, 13,  6, 0)
-  }
-  @org.junit.jupiter.api.Test
-  void getTemperature() {
+    weatherReading1 = new WeatherReading(23, 12,  3, 12);
   }
 
-  @org.junit.jupiter.api.Test
-  void getDewPoint() {
+  @org.junit.Test
+  public void getTemperature() {
+    System.out.println(weatherReading1.getTemperature());
   }
 
-  @org.junit.jupiter.api.Test
-  void getWindSpeed() {
+  @org.junit.Test
+  public void getDewPoint() {
+    System.out.println(weatherReading1.getDewPoint());
   }
 
-  @org.junit.jupiter.api.Test
-  void getTotalRain() {
+  @org.junit.Test
+  public void getWindSpeed() {
+    System.out.println(weatherReading1.getWindSpeed());
   }
 
-  @org.junit.jupiter.api.Test
-  void getRelativeHumidity() {
+  @org.junit.Test
+  public void getTotalRain() {
+    System.out.println(weatherReading1.getTotalRain());
+  }
+
+  @org.junit.Test
+  public void getRelativeHumidity() {
+    System.out.println(weatherReading1.getRelativeHumidity());
+  }
+
+  @org.junit.Test
+  public void getHeatIndex() {
+    System.out.println(weatherReading1.getHeatIndex());
+  }
+
+  @org.junit.Test
+  public void getWindChill() {
+    System.out.println(weatherReading1.getWindChill());
+  }
+
+  @org.junit.Test
+  public void PrintReading() {
+    System.out.println(weatherReading1);
   }
 }
