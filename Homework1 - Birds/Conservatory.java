373d420dbf64fe2255d17a68371fe9ef4edadb11
Casca Kwok <casca.kwok@gmail.com>
2022-10-02 11:23:19 -0700

    Add files via upload

--- Homework1 - Birds/Conservatory.java
+++ Homework1 - Birds/Conservatory.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Conservatory {
-    Birds bird;
+    private Birds bird;
     private int index = 0;
-    Map<Birds, OtherAviary> map = new HashMap<>();
-    Map<String, ArrayList<String>> birdAssignment_map = new HashMap<String, ArrayList<String>>();
-    OtherAviary birdIndex[] = new OtherAviary[100];
+
+    private Map<Birds, OtherAviary> map = new HashMap<>();
+    private static Map<String, ArrayList<String>> birdAssignment_map = new HashMap<String, ArrayList<String>>();
+    private OtherAviary birdIndex[] = new OtherAviary[100];
     public Conservatory(Birds bird) throws IllegalArgumentException {
         this.bird = bird;
     }
@@ -15,11 +17,10 @@ public class Conservatory {
     public Map<String, ArrayList<String>> AssignAviary(){
         if (bird.getClass().equals(BirdsofPrey.class)) {
             birdIndex[index] = new OtherAviary(this.bird);
-            map.put(this.bird, birdIndex[index]);
+            //map.put(this.bird, birdIndex[index]);
             birdAssignment_map.put(this.bird.GetName(), new ArrayList<String>());
             birdAssignment_map.get(this.bird.GetName()).add(birdIndex[index].GetAviaryAssignment());
             birdAssignment_map.get(this.bird.GetName()).add(birdIndex[index].GetLocation());
-            return birdAssignment_map;
         }
         return birdAssignment_map;
     }
@@ -27,6 +28,12 @@ public class Conservatory {
     public String GetAviaryNum() {
         return this.bird.GetName() + birdAssignment_map.get(this.bird.GetName());
     }
-
-
+    public static Map<String, ArrayList<String>> GetMap() {
+        return OtherAviary.GetOtherAviaryMap();
+    }
+    public static Map<String, ArrayList<String>> GetBirdIndex() {
+        TreeMap<String, ArrayList<String>> sorted_birdAssignment_map = new TreeMap<>();
+        sorted_birdAssignment_map.putAll(birdAssignment_map);
+        return sorted_birdAssignment_map;
+    }
 }
--- Homework1 - Birds/OtherAviary.java
+++ Homework1 - Birds/OtherAviary.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class OtherAviary extends Conservatory{
     private static int occupancy = 0;
     private static int aviaryNum = 0;
     private String location;
     private static List<String> aviaryDesc = new ArrayList<String>();
+
+    private static Map<String, ArrayList<String>> GetOtherAviaryMap; static {
+        GetOtherAviaryMap = new HashMap<String, ArrayList<String>>();
+        GetOtherAviaryMap.put("Ground Floor", new ArrayList<String>());
+        GetOtherAviaryMap.put("First Floor", new ArrayList<String>());
+    }
     public OtherAviary(Birds bird){
+
         super(bird);
 
         if (bird.getClass().equals(flightlessBirds.class)){
@@ -16,9 +26,8 @@ public class OtherAviary extends Conservatory{
 
         if (occupancy == 1) {
             aviaryDesc.add(bird.GetCharacteristics());
-            System.out.println(bird.GetCharacteristics());
         }
-        else if (occupancy <=5) {
+        if (occupancy <=5) {
             aviaryNum = 0;
             location = "Ground Floor";
         }
@@ -26,24 +35,25 @@ public class OtherAviary extends Conservatory{
             aviaryNum = 1;
             location = "First Floor";
         }
+        GetOtherAviaryMap.get(location).add(GetAviaryAssignment());
+        GetOtherAviaryMap.get(location).add(bird.GetName());
 
         if (aviaryDesc.contains(bird.GetCharacteristics()) == false) {
             aviaryDesc.add(bird.GetCharacteristics());
         }
     }
-    public static int ReturnAviaryNum() {
-        return aviaryNum;
-    }
+
     public String GetAviaryAssignment() {
         return "OtherAviary" + aviaryNum;
-    }
-    public static int GetOccupancy() {
-        return occupancy;
     }
     public String GetLocation() {
         return location;
     }
     public static List<String> GetDesc() {
         return aviaryDesc;
+    }
+    public static Map<String, ArrayList<String>> GetOtherAviaryMap() {
+        System.out.println("GetOtherAviaryMap: " + GetOtherAviaryMap);
+        return GetOtherAviaryMap;
     }
 }
